2fe3c82d7566ec9dbd3da598de91e08b401a4f15
croraf <vrafaeli@msn.com>
2019-01-07 23:49:50 +0100

    Handle for server not to crash when websocket hello message fails

--- backend/src/ws/index.js
+++ backend/src/ws/index.js
@@ -43,21 +43,26 @@ const configureWss = async (server) => {
             if(data.type === 'hello') {
                 // When the ws client connects, he should send authorization jwt
                 // If the credentials are OK, add the connection to the connections map
-                const credentials = jwt.verify(data.message, 'abcdef');
-                console.log('[WSS] credentials:', credentials);
-                const userId = credentials.id;
-                // Websockets for specific user are stored in a list, as user can open multiple instances 
-                // (on several devices or browsers) and each should receive updates
-                if (!authorizedWebsockets[userId]) {authorizedWebsockets[userId] = [];}
-                authorizedWebsockets[userId].push(ws);
-                console.log('[WSS] added socket in the list of sockets for connected user');
+                try {
+                    const credentials = jwt.verify(data.message, 'abcdef');
 
-                ws.on('close', (code) => {
-                    console.log('[WSS] connection for user:', userId, ', closed with code:', code);
-                    // remove websocket from user's list of websockets
-                    authorizedWebsockets[userId] = authorizedWebsockets[userId].filter(element => element !== ws);
-                    console.log('[WSS] user:', userId, ', has:', authorizedWebsockets[userId].length, 'ws connections remaining');
-                });
+                    console.log('[WSS] credentials:', credentials);
+                    const userId = credentials.id;
+                    // Websockets for specific user are stored in a list, as user can open multiple instances 
+                    // (on several devices or browsers) and each should receive updates
+                    if (!authorizedWebsockets[userId]) {authorizedWebsockets[userId] = [];}
+                    authorizedWebsockets[userId].push(ws);
+                    console.log('[WSS] added socket in the list of sockets for connected user');
+
+                    ws.on('close', (code) => {
+                        console.log('[WSS] connection for user:', userId, ', closed with code:', code);
+                        // remove websocket from user's list of websockets
+                        authorizedWebsockets[userId] = authorizedWebsockets[userId].filter(element => element !== ws);
+                        console.log('[WSS] user:', userId, ', has:', authorizedWebsockets[userId].length, 'ws connections remaining');
+                    });
+                } catch (err) {
+                    console.log('[WSS] processing "hello" message failed:', err);
+                }
             }
         });
     });
