5036d7508365dcdbcc33db422f70a9a21b503bee
croraf <vrafaeli@msn.com>
2019-02-05 12:48:26 +0100

    Reshape restaurants to contain prices as numbers instead of strings with comma

--- backend/tools/restaurants.js
+++ backend/tools/restaurants.js
@@ -6,13 +6,13 @@ const restaurantsList = [
         area: 'Centar',
         rating: 4.0,
         foods: [
-            {name: 'Bečki odrezak', price: '25,50', description: 'Pohani svinjski odrezak'},
-            {name: 'Pizza margherita', price: '32,00', description: 'Sir, Umak od rajčice, Masline, Bosiljak'},
-            {name: 'Pizza capricciosa', price: '39,00', description: 'Sir, Šunka, Umak od rajčice, Gljive, Masline, Bosiljak'},
-            {name: 'Hamburger', price: '25,00', description: 'Mljevena junetina, salata, hambi umak, krastavci, rajčica'},
-            {name: 'Cheeseburger', price: '28,00', description: 'Mljevena junetina, salata, hambi umak, krastavci, rajčica, sir'},
-            {name: 'Pileći file na žaru', price: '32,00', description: 'Pileći file, pecivo'},
-            {name: 'Svinjski odrezak na žaru', price: '30,00', description: 'Svinjski odrezak, pecivo'}
+            {name: 'Bečki odrezak', price: 25.50, description: 'Pohani svinjski odrezak'},
+            {name: 'Pizza margherita', price: 32.00, description: 'Sir, Umak od rajčice, Masline, Bosiljak'},
+            {name: 'Pizza capricciosa', price: 39.00, description: 'Sir, Šunka, Umak od rajčice, Gljive, Masline, Bosiljak'},
+            {name: 'Hamburger', price: 25.00, description: 'Mljevena junetina, salata, hambi umak, krastavci, rajčica'},
+            {name: 'Cheeseburger', price: 28.00, description: 'Mljevena junetina, salata, hambi umak, krastavci, rajčica, sir'},
+            {name: 'Pileći file na žaru', price: 32.00, description: 'Pileći file, pecivo'},
+            {name: 'Svinjski odrezak na žaru', price: 30.00, description: 'Svinjski odrezak, pecivo'}
         ]
     },
     {
@@ -21,7 +21,7 @@ const restaurantsList = [
         area: 'Trešnjevka',
         rating: 4.1,
         foods: [
-            {name: 'Bečki odrezak', price: '25,50', description: 'Pohani svinjski odrezak'}
+            {name: 'Bečki odrezak', price: 25.50, description: 'Pohani svinjski odrezak'}
         ]
     },
     {
@@ -30,9 +30,9 @@ const restaurantsList = [
         area: 'Trnje',
         rating: 3.5,
         foods: [
-            {name: 'Bečki odrezak', price: '25,50', description: 'Pohani svinjski odrezak'},
-            {name: 'Hamburger', price: '25,00', description: 'Mljevena junetina, salata, hambi umak, krastavci, rajčica'},
-            {name: 'Cheeseburger', price: '28,00', description: 'Mljevena junetina, salata, hambi umak, krastavci, rajčica, sir'},
+            {name: 'Bečki odrezak', price: 25.50, description: 'Pohani svinjski odrezak'},
+            {name: 'Hamburger', price: 25.00, description: 'Mljevena junetina, salata, hambi umak, krastavci, rajčica'},
+            {name: 'Cheeseburger', price: 28.00, description: 'Mljevena junetina, salata, hambi umak, krastavci, rajčica, sir'},
         ]
     },
     {
@@ -41,11 +41,11 @@ const restaurantsList = [
         area: 'Trešnjevka',
         rating: 4.5,
         foods: [
-            {name: 'Bečki odrezak', price: '25,50', description: 'Pohani svinjski odrezak'},
-            {name: 'Svinjski odrezak na žaru', price: '30,00', description: 'Svinjski odrezak, pecivo'},
-            {name: 'Pečeno carsko meso i restani krumpir', price: '30,00', description: 'Svinjsko meso, restani krumpir'},
-            {name: 'Hamburger', price: '25,00', description: 'Mljevena junetina, salata, hambi umak, krastavci, rajčica'},
-            {name: 'Cheeseburger', price: '28,00', description: 'Mljevena junetina, salata, hambi umak, krastavci, rajčica, sir'},
+            {name: 'Bečki odrezak', price: 25.50, description: 'Pohani svinjski odrezak'},
+            {name: 'Svinjski odrezak na žaru', price: 30.00, description: 'Svinjski odrezak, pecivo'},
+            {name: 'Pečeno carsko meso i restani krumpir', price: 30.00, description: 'Svinjsko meso, restani krumpir'},
+            {name: 'Hamburger', price: 25.00, description: 'Mljevena junetina, salata, hambi umak, krastavci, rajčica'},
+            {name: 'Cheeseburger', price: 28.00, description: 'Mljevena junetina, salata, hambi umak, krastavci, rajčica, sir'},
         ]
     }, 
     {
@@ -54,7 +54,7 @@ const restaurantsList = [
         area: 'Trešnjevka',
         rating: 3.5,
         foods: [
-            {name: 'Bečki odrezak', price: '25,50', description: 'Pohani svinjski odrezak'}
+            {name: 'Bečki odrezak', price: 25.50, description: 'Pohani svinjski odrezak'}
         ]
     }, 
     {
@@ -63,7 +63,7 @@ const restaurantsList = [
         area: 'Centar',
         rating: 3.7,
         foods: [
-            {name: 'Bečki odrezak', price: '25,50', description: 'Pohani svinjski odrezak'}
+            {name: 'Bečki odrezak', price: 25.50, description: 'Pohani svinjski odrezak'}
         ]
     }, 
     {
@@ -72,7 +72,7 @@ const restaurantsList = [
         area: 'Centar',
         rating: 3.7,
         foods: [
-            {name: 'Bečki odrezak', price: '25,50', description: 'Pohani svinjski odrezak'}
+            {name: 'Bečki odrezak', price: 25.50, description: 'Pohani svinjski odrezak'}
         ]
     }
     /* { name: 'Chello', meals: ['Pizza Hellas', 'Pizza Chello'] } */
