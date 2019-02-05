122958e4a32c7266ab2068d4d00fb525a358f097
croraf <vrafaeli@msn.com>
2019-02-05 12:38:03 +0100

    Refactor of FoodItems file into 2 files

--- frontend/src/components/FoodItems/FoodItems.js
+++ frontend/src/components/FoodItems/FoodItems.js
@@ -1,51 +1,5 @@
 import React from 'react';
-import CloseIcon from '@material-ui/icons/Close';
-
-class FoodItem extends React.Component {
-    render () {
-        const {foodItem, editable} = this.props;
-
-        const closeButton = editable && (
-            <div style={{cursor: 'pointer'}}>
-                <CloseIcon />
-            </div>
-        );
-
-        return (
-            <li style={{
-                borderBottom: '1px solid black',
-                marginTop: '10px',
-                paddingBottom: '5px'
-            }}>
-                <div style={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    width: '100%'
-                }}>
-                    <div>{foodItem}</div>
-                    {closeButton}
-                </div>
-                <div style={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    width: '100%'
-                }}>
-                    <div style={{
-                        display: 'flex'
-                    }}>
-                        {editable && <div style={{cursor: 'pointer'}}>-</div>}
-                        <div style={{paddingLeft: '10px', paddingRight: '10px'}}>2</div>
-                        {editable && <div style={{cursor: 'pointer'}}>+</div>}
-                    </div>
-                    
-                    50,00kn
-                    
-                </div>
-            </li>
-        );
-    }
-}
-
+import {FoodItem} from './FoodItem';
 class FoodItems extends React.Component {
 
     render() {
